807979bfcf8803c6ce37e8d4d72eaca38561d2b0
apple <625543788@qq.com>
2020-03-08 15:44:00 -0400

    server completed

--- code/server.py
+++ code/server.py
@@ -22,19 +22,17 @@ def user_detailed_information():
         #127.0.0.1:5000/user?user_id=__
     else:
         return "Error: No id field provided. Please specify an id."
-    results = []
     mycursor.execute("select * from user where userID=\""+id+"\"")
-    results = mycursor.fetchall()
+    results=get_results(mycursor)
     return jsonify(results)
 
-@app.route('/friends', methods=['GET'])
+@app.route('/friends_post', methods=['GET'])
 def friends_post():
     if 'user_id' in request.args:
         id = str(request.args['user_id'])
     else:
         return "Error: Cannot find the user id."
-    results=[]
-    mycursor.execute("select nickname,postID,post_time,content from "
+    mycursor.execute("select * from "
                      "(select * from post where userID in "
                      "(select followingID from friendsTable "
                      "where followerID = \'"+id+"\')"
@@ -42,13 +40,99 @@ def friends_post():
                      "(select postID from statusTable where userID="
                      "\'"+id+"\' and seen=true)) as A "
                      "inner join user on A.userID=user.userID;")
-    results=mycursor.fetchall()
+    results=get_results(mycursor)
+    mark_seen(id,results,mycursor)
+    return jsonify(results)
+
+@app.route('/topic_post', methods=['GET'])
+def topic_post():
+    if 'user_id' in request.args:
+        id = str(request.args['user_id'])
+    else:
+        return "Error: Cannot find the user id."
+    mycursor.execute("select * from post where postID in "
+                     "(select postID from postTopicTable "
+                     "where topicID in (select topicID "
+                     "from userTopicTable where userID"
+                     " = \'"+id+"\')) and postID not in "
+                     "(select postID from statusTable where userID="
+                     "\'"+id+"\' and seen=true)")
+    results = get_results(mycursor)
+    mark_seen(id, results, mycursor)
+    return jsonify(results)
+@app.route('/group_post', methods=['GET'])
+def group_post():
+    if 'user_id' in request.args:
+        id = str(request.args['user_id'])
+    else:
+        return "Error: Cannot find the user id."
+    mycursor.execute("select * from post where userID in "
+                     "(select userID from userGroupTable "
+                     "where groupID in (select groupID "
+                     "from userGroupTable where userID"
+                     " = \'" + id + "\')) and postID not in "
+                                    "(select postID from statusTable where userID="
+                                    "\'" + id + "\' and seen=true)")
+    results = get_results(mycursor)
+    mark_seen(id, results, mycursor)
+    return jsonify(results)
+
+@app.route('/all_topic', methods=['GET'])
+def all_topic():
+    if 'topic_id' in request.args:
+        id = str(request.args['topic_id'])
+    else:
+        return "Error: Cannot find the topic id."
+    mycursor.execute("select nickname,post_time,content"
+                     " from (select * from post where postID in "
+                     "(select postID from postTopicTable where topicID=\""+id+"\"))"
+                     " as A inner join user on A.userID=user.userID;")
+    results = get_results(mycursor)
+    return jsonify(results)
+
+@app.route('/all_person',methods=['GET'])
+def all_person():
+    if 'user_id' in request.args:
+        id = str(request.args['user_id'])
+    else:
+        return "Error: Cannot find the user id."
+    mycursor.execute("select * from post where userID=\""+id+"\";")
+    results = get_results(mycursor)
+    return jsonify(results)
+
+@app.route('/friend_list',methods=['GET'])
+def friend_list():
+    if 'user_id' in request.args:
+        id = str(request.args['user_id'])
+    else:
+        return "Error: Cannot find the user id."
+    mycursor.execute("select userID,nickname from user where userID in (select followingID from friendsTable where followerID=\'" + id + "\');")
+    results = get_results(mycursor)
+    return jsonify(results)
+
+@app.route('/all_group',methods=['GET'])
+def all_group():
+    mycursor.execute("select * from peopleGroup;")
+    results = get_results(mycursor)
+    return jsonify(results)
+
+def mark_seen(id,results,mycursor):
     for r in results:
-        mycursor.execute("select * from statusTable where userID=\""+id+"\" and postID=\""+r[1]+"\";")
+        mycursor.execute("select * from statusTable where userID=\""+id+"\" and postID=\""+r['postID']+"\";")
         t=mycursor.fetchall()
         if t == []:
-            mycursor.execute("insert into statusTable values (\""+id+"\",\""+r[1]+"\",NULL,NULL,true,\""+str(datetime.datetime.now())+"\")")
+            mycursor.execute("insert into statusTable values (\""+id+"\",\""+r['postID']+"\",NULL,NULL,true,\""+str(datetime.datetime.now())+"\")")
         else:
-            mycursor.execute("update statusTable set seen=true, seenTime=\""+str(datetime.datetime.now())+"\" where userID=\""+id+"\" and postID=\""+r[1]+"\";")
-    return jsonify(results)
+            mycursor.execute("update statusTable set seen=true, seenTime=\""+str(datetime.datetime.now())+"\" where userID=\""+id+"\" and postID=\""+r['postID']+"\";")
+
+def get_results(db_cursor):
+    desc = [d[0] for d in db_cursor.description]
+    results = [dotdict(dict(zip(desc, res))) for res in db_cursor.fetchall()]
+    return results
+
+class dotdict(dict):
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+
 app.run()
